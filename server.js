61349df8f80c6fef4d9ab1fdc02d7316ac7fdcbf
cardfaux <cardfaux@tutanota.com>
2020-07-25 11:23:02 -0500

    added colors for the console logs

--- server.js
+++ server.js
@@ -2,17 +2,42 @@
 const Koa = require("koa");
 const koaRouter = require("koa-router");
 
+// Other File Imports
+const colors = require("./colors");
+
 // Instances of modules
 const app = new Koa();
 const router = new koaRouter();
 
 // Constants
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //Middlewares
 // app.use(async (ctx) => {
 //   ctx.body = "Hello James";
 // });
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get("X-Response-Time");
+  console.log(
+    colors.fg.Magenta,
+    colors.Reverse,
+    `${ctx.method} ${ctx.url} - ${rt}`,
+    colors.Reset,
+  );
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
 
 //Middlewares
 app.use(router.routes());
@@ -20,6 +45,7 @@ app.use(router.allowedMethods());
 
 // Routes
 router.get("/about", async (ctx) => {
+  console.log(ctx);
   return (
     ctx.body = "<h1>About Page</h1>"
   );
@@ -37,5 +63,5 @@ router.get("/gallery", async (ctx) => {
 
 // Start The Server
 app.listen(PORT, () => {
-  console.log(`\x1b[36m%s\x1b[0m`, `Server Is Running On Port ${PORT}`);
+  console.log(colors.fg.Cyan, `Server Is Running On Port ${PORT}`);
 });
